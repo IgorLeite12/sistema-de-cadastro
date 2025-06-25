d868706399225608c0628203b69ad154648f3620
igor.leite <igor.leite@fpf.br>
2025-06-25 17:56:17 -0400

    feat: update registration form and navigation, add Angular Flex Layout and Material components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ConsultationComponent } from './consultation/consultation.component';
+import { RegistrationComponent } from './registration/registration.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'clientes', component: ConsultationComponent},
+    {path: 'cadastro', component: RegistrationComponent},
+];
